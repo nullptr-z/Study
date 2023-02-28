48e8d1ea72b7f1f2af6285137bc927791f042c56
zhouzheng <zhouzheng0646@qq.com>
2023-03-01 01:32:55 +0800

    2023-03-01 01:32:54

--- Rust/webcam/src/components/controls.rs
+++ Rust/webcam/src/components/controls.rs
@@ -1,5 +1,7 @@
 use sycamore::prelude::*;
 use tracing::info;
+use wasm_bindgen::JsCast;
+use web_sys::{console, Event, HtmlSelectElement};
 
 use crate::{AppState, Device};
 
@@ -11,7 +13,15 @@ pub fn Controls<G: Html>(ctx: BoundedScope) -> View<G> {
 
     view! {ctx,
         div(class="absolute  bottom-2 p-5 "){
-            select(class="bg-blue-500 rounded-sm p-1" ) {
+            select(
+                class="bg-blue-500 rounded-sm p-1",
+                on:click=|e: Event|{
+                    let target=e.target().unwrap().unchecked_into::<HtmlSelectElement>();
+                    let device_id=target.value();
+                    info!("device_id: {}",&device_id);
+                    state.device_id.set(device_id);
+                }
+            ) {
                 Keyed (
                   iterable= devices,
                   key=|device| device.id.clone(),
