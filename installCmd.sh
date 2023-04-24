8edcd42ea547efaf18904d4d677b9a96fac5ef45
zhouzheng <zhouzheng0646@qq.com>
2023-04-24 16:26:40 +0800

    2023-04-24

--- installCmd.sh
+++ installCmd.sh
@@ -3,4 +3,9 @@ echo "alias hotkey='cat ~/Study/hotKey'" >> ~/.zshrc
 echo "alias cargow='cargo watch -x run'" >> ~/.zshrc
 mv ~/.ssh/config ~/.ssh/config_remarks
 ln -s $PWD/ssh_config  ~/.ssh/config
-	source ~/.zshrc
+
+## 别名
+alias gitdayline='git log --author=zhouzheng --since=1.day.ago --shortstat'
+alias gitdaylines='git log --shortstat --author=zhouzheng --since=midnight | grep -E "insertions|deletion"'
+
+source ~/.zshrc
