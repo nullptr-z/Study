86a4978b4f45873259e6867ab2bbde94850efe3d
WmW <zhouzheng0646@qq.com>
2023-04-25 22:28:40 +0800

    2023-04-25

--- installCmd.sh
+++ installCmd.sh
@@ -5,7 +5,8 @@ mv ~/.ssh/config ~/.ssh/config_remarks
 ln -s $PWD/ssh_config  ~/.ssh/config
 
 ## 别名
-alias gitdayline='git log --author=zhouzheng --since=1.day.ago --shortstat'
+alias gitdayline='git log --author=zhouzheng --since=2.day.ago --shortstat'
 alias gitdaylines='git log --shortstat --author=zhouzheng --since=midnight | grep -E "insertions|deletion"'
+alias gitdayRange='git log --shortstat --author=zhouzheng --since="2023-04-25" --until="2023-04-26"| grep -E "Date|insertions|deletion"'
 
 source ~/.zshrc
