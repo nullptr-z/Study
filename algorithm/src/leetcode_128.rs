4a9fab21ddede540b7b5e6de7cbc891ff564fdfc
WmW <zhouzheng0646@qq.com>
2023-10-24 13:03:28 +0800

    feat: practice questions for leetcode  128

---
+++ algorithm/src/leetcode_128.rs
@@ -0,0 +1,43 @@
+//
+
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
+        let mut map = HashMap::new();
+        let mut max = 0;
+
+        for value in nums.iter() {
+            if map.get(value).is_some() {
+                continue;
+            }
+            let pre = map.get(&(value - 1)).copied().unwrap_or(0);
+            let next = map.get(&(value + 1)).copied().unwrap_or(0);
+
+            let val = pre + next + 1;
+            max = max.max(val);
+
+            // - pre和+next一定会找到map元素中存在的
+            // pre和next存储的值就是，距离当前值的距离
+            map.insert(*value, val);
+            map.insert(value - pre, val);
+            map.insert(value + next, val);
+        }
+
+        max
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    #[test]
+    fn should_work() {
+        Solution::longest_consecutive(vec![100, 4, 200, 1, 3, 2]);
+
+        Solution::longest_consecutive(vec![0, 3, 7, 2, 5, 8, 4, 6, 0, 1]);
+    }
+}
+
+pub struct Solution;
