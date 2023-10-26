1fc198fdd428d825e12a6a3283cfce37f52d2529
WmW <zhouzheng0646@qq.com>
2023-10-26 22:22:38 +0800

    feat: practice questions for leetcode  146

--- algorithm/src/leetcode_146.rs
+++ algorithm/src/leetcode_146.rs
@@ -6,7 +6,6 @@ struct LRUCache {
     activity_level: BTreeMap<i32, i32>,
     operator_count: i32,
     capacity: i32,
-    min: (i32, i32),
 }
 
 impl LRUCache {
@@ -18,7 +17,6 @@ impl LRUCache {
             activity_level: BTreeMap::new(),
             operator_count: 0,
             capacity: capacity,
-            min: (0, 0),
         }
     }
 
@@ -29,12 +27,6 @@ impl LRUCache {
             self.activity_level
                 .remove(&self.cache[key as usize].unwrap().1);
             self.activity_level.insert(self.operator_count, key);
-
-            let f = self.activity_level.iter().next();
-            let k = *f.unwrap().0;
-            let v = *f.unwrap().1;
-            self.min = (k, v);
-
             self.cache[key as usize] = Some((item.0, self.operator_count));
             return item.0;
         }
@@ -49,12 +41,6 @@ impl LRUCache {
             self.activity_level
                 .remove(&self.cache[key as usize].unwrap().1);
             self.activity_level.insert(self.operator_count, key);
-
-            let f = self.activity_level.iter().next();
-            let k = *f.unwrap().0;
-            let v = *f.unwrap().1;
-            self.min = (k, v);
-
             self.cache[key as usize] = Some((value, self.operator_count));
             return;
         }
@@ -65,24 +51,15 @@ impl LRUCache {
             //     self.capacity += 1;
             // }
 
-            // if let Some((&key, value)) = self.activity_level.clone().iter().next() {
-            //     self.activity_level.remove(&key);
-            //     self.cache[*value as usize] = None;
-            //     self.capacity += 1;
-            // }
-            let min = self.min;
-            self.activity_level.remove(&min.0);
-            self.cache[min.1 as usize] = None;
+            let f = self.activity_level.iter().next();
+            let k = *f.unwrap().0;
+            let v = *f.unwrap().1;
+            self.activity_level.remove(&k);
+            self.cache[v as usize] = None;
         }
 
         self.capacity -= 1;
         self.activity_level.insert(self.operator_count, key);
-
-        let f = self.activity_level.iter().next();
-        let k = *f.unwrap().0;
-        let v = *f.unwrap().1;
-        self.min = (k, v);
-
         self.cache[key as usize] = Some((value, self.operator_count))
     }
 }
