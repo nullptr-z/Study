61df1317120550c2021be3539d536e1832703b76
WmW <zhouzheng0646@qq.com>
2023-11-01 22:26:02 +0800

    feat: practice questions for leetcode  153

--- algorithm/src/leetcode_153.rs
+++ algorithm/src/leetcode_153.rs
@@ -14,6 +14,23 @@ impl Solution {
 
         nums[l]
     }
+
+    pub fn find_min_lt(nums: Vec<i32>) -> i32 {
+        let mut len = nums.len();
+        let mut l = 0;
+        let mut r = len;
+        while l < r {
+            let mid = (r + l) >> 1;
+            if nums[mid] <= nums[len - 1] {
+                r = mid
+            } else {
+                l = mid + 1
+            }
+        }
+
+        println!("【 l 】==> {:?} {}", l, r);
+        nums[l]
+    }
 }
 
 #[cfg(test)]
