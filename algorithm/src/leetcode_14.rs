b875e1a6c816c82c4bf7d76cedc140934951b84d
WmW <zhouzheng0646@qq.com>
2023-11-01 23:14:34 +0800

    feat: practice questions for leetcode  14

--- algorithm/src/leetcode_14.rs
+++ algorithm/src/leetcode_14.rs
@@ -19,6 +19,39 @@ impl Solution {
 
         prefix
     }
+
+    pub fn longest_common_prefix_s(strs: Vec<String>) -> String {
+        let mut s = "".to_string();
+
+        let chats: Vec<Vec<char>> = strs.into_iter().map(|m| m.chars().collect()).collect();
+
+        let mut i = 0;
+        loop {
+            let mut flag = true;
+            if i >= chats[0].len() {
+                break;
+            }
+            let ch = chats[0][i];
+            for item_str in chats.iter() {
+                if i >= item_str.len() {
+                    flag = false;
+                    break;
+                } else {
+                    flag = flag && item_str[i] == ch;
+                }
+            }
+
+            if flag {
+                s += &ch.to_string();
+            } else {
+                break;
+            }
+
+            i += 1;
+        }
+
+        s.to_string()
+    }
 }
 
 #[cfg(test)]
