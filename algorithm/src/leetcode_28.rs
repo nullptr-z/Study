e676204353c3b06b47fb582d4df0c8aed3a28ceb
WmW <zhouzheng0646@qq.com>
2023-11-02 22:56:32 +0800

    feat: practice questions for leetcode  28

---
+++ algorithm/src/leetcode_28.rs
@@ -0,0 +1,48 @@
+impl Solution {
+    pub fn str_str(haystack: String, needle: String) -> i32 {
+        if haystack.len() < needle.len() {
+            return -1;
+        }
+
+        let haystack = haystack.as_bytes();
+        let needle = needle.as_bytes();
+
+        let mut start = 0;
+        let mut i = 0;
+        while i < haystack.len() {
+            if start == needle.len() {
+                break;
+            }
+            if haystack[i] == needle[start] {
+                i += 1;
+                start += 1;
+                continue;
+            }
+
+            if start > 0 {
+                i = i - start + 1;
+            } else {
+                i += 1;
+            }
+            start = 0;
+        }
+        if start <= needle.len() - 1 {
+            return -1;
+        }
+
+        (i - start) as i32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    #[test]
+    fn should_work() {
+        let r = Solution::str_str("mississippi".to_string(), "a".to_string());
+        println!("【 r 】==> {:?}", r);
+    }
+}
+
+pub struct Solution;
--- algorithm/src/lib.rs
+++ algorithm/src/lib.rs
@@ -50,6 +50,7 @@ mod leetcode_236;
 mod leetcode_26;
 mod leetcode_27;
 mod leetcode_279;
+mod leetcode_28;
 mod leetcode_322;
 mod leetcode_33;
 mod leetcode_34;
