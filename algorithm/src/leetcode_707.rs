4cc766ece01ff4c21ecf84db67ac3392bfa130a5
WmW <zhouzheng0646@qq.com>
2023-12-15 23:42:02 +0800

    leetcode 707

--- algorithm/src/leetcode_707.rs
+++ algorithm/src/leetcode_707.rs
@@ -1,31 +1,104 @@
-// struct MyLinkedList {}
+use std::cell::RefCell;
+use std::rc::Rc;
+struct MyLinkedList {
+    n: i32,
+    head: Rc<RefCell<MyLinkedListNode>>,
+    tail: Rc<RefCell<MyLinkedListNode>>,
+}
 
-// /**
-//  * `&self` means the method takes an immutable reference.
-//  * If you need a mutable reference, change it to `&mut self` instead.
-//  */
-// impl MyLinkedList {
-//     fn new() -> Self {}
+struct MyLinkedListNode {
+    val: i32,
+    next: Option<Rc<RefCell<MyLinkedListNode>>>,
+}
 
-//     fn get(&self, index: i32) -> i32 {}
+impl MyLinkedList {
+    fn new() -> Self {
+        let v_node = Rc::new(RefCell::new(MyLinkedListNode { val: 0, next: None }));
+        Self {
+            n: 0,
+            head: v_node.clone(),
+            tail: v_node.clone(),
+        }
+    }
 
-//     fn add_at_head(&self, val: i32) {}
+    fn get(&self, index: i32) -> i32 {
+        if index < 0 || index >= self.n {
+            return -1;
+        }
+        let mut pre = self.head.borrow().next.clone();
+        for _ in 0..index {
+            pre = pre.and_then(|node| node.borrow().next.clone());
+        }
+        pre.map(|node| node.borrow().val).unwrap()
+    }
 
-//     fn add_at_tail(&self, val: i32) {}
+    fn add_at_head(&mut self, val: i32) {
+        self.add_at_index(0, val);
+    }
 
-//     fn add_at_index(&self, index: i32, val: i32) {}
+    fn add_at_tail(&mut self, val: i32) {
+        let new_node = Rc::new(RefCell::new(MyLinkedListNode { val, next: None }));
+        self.tail.borrow_mut().next = Some(new_node.clone());
+        self.tail = new_node;
+        self.n += 1;
+    }
 
-//     fn delete_at_index(&self, index: i32) {}
-// }
+    fn add_at_index(&mut self, index: i32, val: i32) {
+        if index < 0 || index > self.n {
+            return;
+        }
+        if index == self.n {
+            self.add_at_tail(val);
+            return;
+        }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::Solution;
-//     use crate::list_utils::arrayToList;
+        let mut pre = Some(self.head.clone());
+        for _ in 0..index {
+            pre = pre.and_then(|node| node.borrow().next.clone());
+        }
+        let next = pre.clone().and_then(|node| node.borrow().next.clone());
+        let new = Some(Rc::new(RefCell::new(MyLinkedListNode { val, next })));
+        pre.map(|node| node.borrow_mut().next = new);
+        self.n += 1;
+    }
 
-//     #[test]
-//     fn should_work() {}
-// }
+    fn delete_at_index(&mut self, index: i32) {
+        if index < 0 || index >= self.n {
+            return;
+        }
+        let mut pre = Some(self.head.clone());
+        for _ in 0..index {
+            pre = pre.and_then(|node| node.borrow().next.clone());
+        }
+        let next = pre
+            .clone()
+            .and_then(|node| node.borrow().next.clone())
+            .and_then(|node| node.borrow().next.clone());
+        pre.clone().map(|node| node.borrow_mut().next = next);
+        if index == self.n - 1 {
+            self.tail = pre.unwrap();
+        }
+        self.n -= 1;
+    }
+}
 
-// pub struct Solution;
-// use crate::list_utils::ListNode;
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * let obj = MyLinkedList::new();
+ * let ret_1: i32 = obj.get(index);
+ * obj.add_at_head(val);
+ * obj.add_at_tail(val);
+ * obj.add_at_index(index, val);
+ * obj.delete_at_index(index);
+ */
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+    use crate::list_utils::arrayToList;
+
+    #[test]
+    fn should_work() {}
+}
+
+pub struct Solution;
+use crate::list_utils::ListNode;
