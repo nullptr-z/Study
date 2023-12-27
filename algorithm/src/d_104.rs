1de1ed6d0b8c3dd6e6849e51a6675f96ee83e821
WmW <zhouzheng0646@qq.com>
2023-12-28 00:18:48 +0800

    leetcode 104

---
+++ algorithm/src/d_104.rs
@@ -0,0 +1,47 @@
+use std::cell::RefCell;
+use std::collections::VecDeque;
+use std::rc::Rc;
+
+impl Solution {
+    pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        let mut depth = 0;
+        let mut stack = VecDeque::new();
+
+        if root.is_some() {
+            stack.push_back(root.unwrap());
+        }
+
+        let mut count = stack.len();
+        while let Some(root) = stack.pop_front() {
+            count -= 1;
+            if let Some(left) = &root.borrow().left {
+                stack.push_back(left.clone());
+            }
+            if let Some(right) = &root.borrow().right {
+                stack.push_back(right.clone());
+            }
+            if count == 0 {
+                depth += 1;
+                count = stack.len();
+            }
+        }
+
+        depth
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{cell::RefCell, rc::Rc};
+
+    use crate::tree_utils::arrayToTree;
+
+    use super::Solution;
+
+    #[test]
+    fn should_work() {}
+}
+
+pub struct Solution;
+
+use crate::tree_utils::TreeNode;
