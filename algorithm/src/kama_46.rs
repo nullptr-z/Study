606eb6be827a0742aaff2858ce9b8c0c0e1928bf
zhouzheng <zhouzheng0646@qq.com>
2024-01-24 15:59:05 +0800

    kame 46 one dime

--- algorithm/src/kama_46.rs
+++ algorithm/src/kama_46.rs
@@ -1,4 +1,5 @@
 impl Solution {
+    // 二维
     pub fn one_two_backpack(weight: Vec<i32>, price: Vec<i32>, n: i32) -> i32 {
         let mut dp = vec![vec![0; n as usize + 1]; weight.len() + 1];
 
@@ -15,6 +16,21 @@ impl Solution {
 
         *dp.last().unwrap().last().unwrap()
     }
+    // 一维，滚动数组
+    pub fn one_two_backpacks(weight: Vec<i32>, price: Vec<i32>, n: i32) -> i32 {
+        let mut dp = vec![0; n as usize + 1];
+
+        for i in 0..weight.len() {
+            for cap in (0..=n as usize).rev() {
+                let remain = (cap as i32) - weight[i];
+                if remain >= 0 {
+                    dp[cap] = dp[cap].max(price[i] + dp[remain as usize])
+                }
+            }
+        }
+
+        *dp.last().unwrap()
+    }
 }
 
 #[cfg(test)]
@@ -23,7 +39,7 @@ mod tests {
 
     #[test]
     fn should_work() {
-        let ret = Solution::one_two_backpack(vec![2, 3, 4], vec![3, 5, 6], 6);
+        let ret = Solution::one_two_backpacks(vec![2, 3, 4], vec![3, 5, 6], 6);
         println!("【 ret 】==> {:?}", ret);
     }
 }
