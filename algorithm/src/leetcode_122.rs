ce49402cd8dd545ad2c0c295b2be910b4e9dad34
zhouzheng <zhouzheng0646@qq.com>
2024-01-30 12:53:05 +0800

    leetcode 122

--- algorithm/src/leetcode_122.rs
+++ algorithm/src/leetcode_122.rs
@@ -2,12 +2,23 @@ impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
         let mut sum = 0;
         for i in 1..prices.len() {
-            if prices[i] > prices[i - 1] {
-                sum += prices[i] - prices[i - 1];
+            let diff = prices[i] - prices[i - 1];
+            if diff > 0 {
+                sum += diff;
             }
         }
 
         sum
+    }
+
+    pub fn max_profit_dp(prices: Vec<i32>) -> i32 {
+        // [买, 不买]
+        let mut dp = vec![-prices[0], 0];
+        for p in prices {
+            dp[0] = dp[0].max(dp[1] - p);
+            dp[1] = dp[1].max(dp[0] + p);
+        }
+        dp[1]
     }
 }
 
@@ -17,7 +28,7 @@ mod tests {
 
     #[test]
     fn should_work() {
-        let ret = Solution::max_profit(vec![7, 1, 5, 3, 6, 4]);
+        let ret = Solution::max_profit_dp(vec![7, 1, 5, 3, 6, 4]);
         println!("【 ret 】==> {:?}", ret);
     }
 }
