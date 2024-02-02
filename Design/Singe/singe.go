7f5350218fb655bc8c9ca1e4c724bca866dc3b35
zhouzheng <zhouzheng0646@qq.com>
2024-02-02 13:55:32 +0800

    Singe Pattern

---
+++ Design/Singe/singe.go
@@ -0,0 +1,24 @@
+// 单例模式
+// 保证一个类只有一个实例对象，可供其他模块使用，只读，不可被更改
+// 不同的语言可能要借用不同方法来封装，const private ..
+
+package main
+
+import "fmt"
+
+type singe struct{}
+
+func (*singe) Show() {
+	fmt.Println("I'm Singe")
+}
+
+var ptr *singe = new(singe)
+
+func GetSinge() *singe {
+	return ptr
+}
+
+func main() {
+	s := GetSinge()
+	s.Show()
+}
