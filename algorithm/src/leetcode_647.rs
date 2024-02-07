c1242a614152643bf2cd0ed3f8e76807e73962d9
zhouzheng <zhouzheng0646@qq.com>
2024-02-07 15:17:40 +0800

    leetcode 647

---
+++ algorithm/src/leetcode_647.rs
@@ -0,0 +1,57 @@
+use std::usize;
+
+impl Solution {
+    // dp
+    pub fn count_substrings_dp(s: String) -> i32 {
+        let ss = s.as_bytes();
+        let mut dp = vec![vec![false; s.len()]; s.len()];
+        let mut res = 0;
+
+        for i in (0..s.len()).rev() {
+            for j in i..s.len() {
+                // j-1 <= 1代表相同位置，字符串本身就是一个回文
+                if ss[i] == ss[j] && (j - i <= 1 || dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    res += 1;
+                }
+            }
+        }
+        res
+    }
+
+    // 双指针
+    pub fn count_substrings(s: String) -> i32 {
+        fn extends(s: &[u8], mut i: usize, mut j: usize) -> i32 {
+            let mut count = 0;
+            while i < s.len() && j < s.len() && s[i] == s[j] {
+                count += 1;
+                j += 1;
+                i = i.wrapping_sub(1);
+            }
+            return count;
+        }
+
+        let ss = s.as_bytes();
+        let mut count = 0;
+
+        for i in 0..s.len() {
+            count += extends(ss, i, i);
+            count += extends(ss, i, i + 1);
+        }
+
+        count
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    #[test]
+    fn should_work() {
+        let ret = Solution::count_substrings("aaa".into());
+        println!("【 ret 】==> {:?}", ret);
+    }
+}
+
+pub struct Solution;
--- algorithm/src/lib.rs
+++ algorithm/src/lib.rs
@@ -207,6 +207,7 @@ mod leetcode_62;
 mod leetcode_63;
 mod leetcode_637;
 mod leetcode_64;
+mod leetcode_647;
 mod leetcode_654;
 mod leetcode_66;
 mod leetcode_669;
