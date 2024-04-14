031bca9404744e2f0d5a463fb4be0e503f34fea1
WmW <zhouzheng0646@qq.com>
2024-04-14 21:08:23 +0800

    leetc 541

---
+++ algorithm/go/541/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	res := reverseStr("abcdefg", 2)
+	fmt.Println("res:", res)
+}
+
+func reverseStr(s string, k int) string {
+	len, bStr := len(s), []byte(s)
+	k2 := 2 * k
+	i := k2
+
+	for {
+		l := i - k2
+		r := l + k - 1
+		if l >= len {
+			break
+		}
+
+		if r >= len {
+			// 剩余不足k，会进入这里
+			r = len - 1
+		}
+
+		for l < r {
+			bStr[l], bStr[r] = bStr[r], bStr[l]
+			l++
+			r--
+		}
+
+		i += k2
+	}
+
+	retStr := string(bStr)
+	return retStr
+}
