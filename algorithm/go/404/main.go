34629ada80683e40415f966971f89fa98b5707bc
zz <zhouzheng0646@qq.com>
2024-05-13 20:08:10 +0800

    leetcode 404

---
+++ algorithm/go/404/main.go
@@ -0,0 +1,27 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func sumOfLeftLeaves(root *TreeNode) int {
+	return concussion(root, false)
+}
+
+func concussion(root *TreeNode, isLeft bool) int {
+	if root == nil {
+		return 0
+	}
+	if root != nil && root.Left == nil && root.Right == nil {
+		if isLeft {
+			return root.Val
+		} else {
+			return 0
+		}
+	}
+	l := concussion(root.Left, true)
+	r := concussion(root.Right, false)
+	return l + r
+}
