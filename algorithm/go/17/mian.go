519de05234b431db0de867f0b4121f5f49ca1896
zz <zhouzheng0646@qq.com>
2024-05-30 14:20:21 +0800

    leetcode 17

--- algorithm/go/17/mian.go
+++ algorithm/go/17/mian.go
@@ -17,12 +17,27 @@ var digitsMap = map[rune]string{
 
 func letterCombinations(digits string) []string {
 	result := make([]string, 0)
-	concussion(digits, &result)
+	concussion(digits, "", &result)
 
 	return result
 }
 
-func concussion(digits string, tmp *[]string) {
+func concussion(digits string, curStr string, result *[]string) {
+	if len(digits) == 0 {
+		if curStr != "" {
+			*result = append(*result, curStr)
+		}
+		return
+	}
+
+	head := rune(digits[0])
+	digits = digits[1:]
+	for _, c := range digitsMap[head] {
+		concussion(digits, curStr+string(c), result)
+	}
+}
+
+func concussion2(digits string, result *[]string) {
 	if len(digits) == 0 {
 		return
 	}
@@ -30,15 +45,15 @@ func concussion(digits string, tmp *[]string) {
 	tmps := make([]string, 0)
 	chars := digitsMap[rune(digits[0])]
 	for _, ch := range chars {
-		if len(*tmp) == 0 {
+		if len(*result) == 0 {
 			tmps = append(tmps, string(ch))
 		} else {
-			for i := 0; i < len(*tmp); i++ {
-				tmps = append(tmps, (*tmp)[i]+string(ch))
+			for i := 0; i < len(*result); i++ {
+				tmps = append(tmps, (*result)[i]+string(ch))
 			}
 		}
 	}
-	*tmp = tmps
+	*result = tmps
 
-	concussion(digits[1:], tmp)
+	concussion2(digits[1:], result)
 }
