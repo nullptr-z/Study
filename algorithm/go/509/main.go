2c193bc87c302150bba53745b7d0698ad9d4d442
zz <zhouzheng0646@qq.com>
2024-06-06 16:28:47 +0800

    leetcode 509

---
+++ algorithm/go/509/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	ret := fib(4)
+	fmt.Println("ret:", ret)
+}
+
+func fib(n int) int {
+	dp := make([]int, n+1)
+	dp[1] = 1
+	for i := 2; i < len(dp); i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+
+	fmt.Println("dp:", dp)
+	return dp[n]
+}
