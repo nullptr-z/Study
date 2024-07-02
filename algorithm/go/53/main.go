3d6b17120325517346cf2a2d4f60e39b18e75f69
zz <zhouzheng0646@qq.com>
2024-07-02 15:05:07 +0800

    leet 53 dp

--- algorithm/go/53/main.go
+++ algorithm/go/53/main.go
@@ -21,3 +21,16 @@ func maxSubArray(nums []int) int {
 	}
 	return maxSum
 }
+
+func maxSubArray_dp(nums []int) int {
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	maxVal := nums[0]
+
+	for i := 1; i < len(nums); i++ {
+		dp[i] = max(dp[i-1]+nums[i], nums[i])
+		maxVal = max(maxVal, dp[i])
+	}
+
+	return maxVal
+}
