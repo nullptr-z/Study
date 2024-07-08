67c55450e594c9dbcddd0931536c6d4e2837ed10
zz <zhouzheng0646@qq.com>
2024-07-08 20:36:25 +0800

    leet 647

---
+++ algorithm/go/647/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	result := countSubstrings("abac")
+	fmt.Println("result:", result)
+}
+
+func countSubstrings(s string) int {
+	len := len(s)
+	dp := make([][]bool, len)
+	for i := 0; i < len; i++ {
+		dp[i] = make([]bool, len)
+	}
+	count := 0
+
+	for i := len; i >= 0; i-- {
+		for j := i; j < len; j++ {
+			if s[i] == s[j] && (j-i <= 1 || dp[i+1][j-1]) {
+				// if s[i] == s[j] && (i < len || dp[i+1][j-1]) {
+				dp[i][j] = true
+				fmt.Println("dp:", dp)
+				count++
+			}
+		}
+	}
+
+	return count
+}
